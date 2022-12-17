ea13ef5a1416231a57f9f70296cbabaf0f946967
Kim Thanh Bui <thanh.kimm.bui@gmail.com>
2022-12-17 12:07:41 -0800

    Add export modules

--- app.js
+++ app.js
@@ -1,56 +1,22 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 const port = 3000;
 
-let items = ["Buy Foot", "Cook Food", "Eat Food"];
-let workItems = [];
+const items = ["Buy Foot", "Cook Food", "Eat Food"];
+const workItems = [];
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(express.static("public"));
 
 app.set("view engine", "ejs");
 
 app.get("/", (rootGetReq, rootGetRes) => {
-  let today = new Date();
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long",
-  };
+  // let day = date.getDay();
+  let day = date.getDate();
 
-  let day = today.toLocaleDateString("en-US", options);
-  /*   var currentDay = today.getDay();
-  var day = "";
-
-  switch (currentDay) {
-    case 0:
-      day = "Sunday";
-      break;
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-      day = "Tuesday";
-      break;
-    case 3:
-      day = "Wednesday";
-      break;
-    case 4:
-      day = "Thursday";
-      break;
-    case 5:
-      day = "Friday";
-      break;
-    case 6:
-      day = "Saturday";
-      break;
-    default:
-      console.log("Error: Current day is equal to: " + currentDay);
-      break;
-  } */
   rootGetRes.render("list", { listTitle: day, newListItems: items });
 });
 
